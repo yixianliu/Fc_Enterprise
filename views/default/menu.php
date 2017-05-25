1170408852ec367690f895a4e062f3bcdb71715d
Administrator <Administrator@YiXianLiu>
2017-05-26 00:13:52 +0800

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- views/default/menu.php
+++ views/default/menu.php
@@ -1,10 +1,9 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+/**
+ * @abstract 菜单模板
+ *
+ */
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\bootstrap\Nav;
