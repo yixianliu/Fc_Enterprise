87b8a202663efb02e8e72ec610b7ac6703e46088
Yxl <zccem@163.com>
2018-03-19 23:03:24 +0800

    创始室主机上传,架构大改变..2018.03.19

--- frontend/views/default/purchase/view.php
+++ frontend/views/default/purchase/view.php
@@ -4,6 +4,7 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use dosamigos\fileupload\FileUploadUI;
+use common\widgets\iConf\ConfList;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Purchase */
@@ -93,120 +94,23 @@ $this->registerJsFile('@web/themes/qijian/js/jqzoom/base.js');
 
                         <hr>
 
-                        <?php if ($offer == true): ?>
-                            <?php if (!empty(Yii::$app->user->identity->user_id)): ?>
+                        <p>
+                            <?= $model->is_type ?>
+                            <?= $model->is_status ?>
+                            <?= $model->num ?>
+                            <?= $model->is_using ?>
+                        </p>
 
-                                <?php
-                                $form = ActiveForm::begin([
-                                    'action' => ['sp-offer/create', 'id' => 'P0', 'type' => 'Purchase'],
-                                    'method' => 'post',
-                                    'id'     => $modelOffer->formName(),
-                                ]);
-                                ?>
-
-                                <p class="right-tar">
-                                    <span class="right-color">提交价格 : </span>
-                                    <?= $form->field($modelOffer, 'price')->textInput()->label(false) ?>
-                                </p>
-
-                                <p>
-
-                                <hr/>
-
-                                <?=
-                                FileUploadUI::widget([
-                                    'model'         => $modelOffer,
-                                    'attribute'     => 'path',
-                                    'url'           => ['upload/image-upload', 'id' => $modelOffer->path, 'type' => 'sp_offer', 'attribute' => 'path'],
-                                    'gallery'       => false,
-                                    'fieldOptions'  => [
-                                        'accept' => 'image/*'
-                                    ],
-                                    'clientOptions' => [
-                                        'maxFileSize'      => 2000000,
-                                        'dataType'         => 'json',
-                                        'maxNumberOfFiles' => 5,
-                                    ],
-
-                                    // ...
-                                    'clientEvents'  => [
-
-                                        'fileuploaddone' => 'function(e, data) {
-                                console.log(e);
-                                console.log(data);
-                                
-                                var html = "";
-                                
-                                var ImagesContent = $("#ImagesContent");
-                                
-                                $.each(data.result.files, function (index, file) {
-                                    html += file.name + \',\';
-                                });
-                                
-                                html += ImagesContent.val();
-                                
-                                ImagesContent.val(html);
-                                
-                                return true;
-                            }',
-                                        'fileuploadfail' => 'function(e, data) {
-                                console.log(e);
-                                console.log(data);
-                            }',
-                                    ],
-                                ]);
-                                ?>
-
-                                <?= $form->field($modelOffer, 'path')->textarea(['id' => 'ImagesContent', 'style' => 'display:none;'])->label(false) ?>
-
-                                <?= $form->field($modelOffer, 'offer_id')->hiddenInput(['value' => $model->purchase_id])->label(false) ?>
-
-                                <hr/>
-
-                                </p>
-
-                                <p>
-                                    <?= Html::submitButton('提交内容', ['class' => 'btn btn-red']) ?>
-                                </p>
-
-                                <br/>
-
-                                <p>
-                                    <?= Yii::$app->view->renderFile('@app/views/default/formMsg.php'); ?>
-                                </p>
-
-                                <?php ActiveForm::end(); ?>
-
-                            <?php else: ?>
-
-                                <h3>请登录后提交内容 !! </h3>
-
-                                <br/>
-
-                                点这里, <a href="<?= Url::to(['member/login']) ?>" title="<?= $model->title ?>">用户登录</a>
-
-                            <?php endif; ?>
-
-                        <?php else: ?>
-
-                            <h2>此采购已被采纳 !!</h2>
-
-                        <?php endif; ?>
 
                     </div>
                     <!-- #产品参数 -->
 
-                    <!-- 商家信息
+                    <!-- 商家信息 -->
                     <div class="tend-right-right">
 
                         <div class="tend-border">
 
-                            <p class="t-title">湛江七建有限公司</p>
-
-                            <p><span class="right-color">联系人 : </span>XXXX</p>
-                            <p><span class="right-color">座机 : </span>XXX-XXXXXXXX</p>
-                            <p><span class="right-color">手机 : </span>XXXXXXXXXXX</p>
-                            <p><span class="right-color">QQ : </span>XXXXXXXXX</p>
+                            <?= ConfList::widget(['config' => [$this->title, 'view']]); ?>
 
                         </div>
 
@@ -239,18 +143,127 @@ $this->registerJsFile('@web/themes/qijian/js/jqzoom/base.js');
                 </div>
                 <!-- #内容,评论 -->
 
+                <hr/>
+
                 <div class="release-message">
-                    <ul class="tab clearfix">
-                        <li onclick="tabs('#comment',0)" class="curr">
-                            发布留言<strong></strong>
-                        </li>
-                    </ul>
 
-                    <div class="message-cont">
-                        <textarea></textarea>
-                    </div>
+                    <?php if ($offer == true): ?>
+                        <?php if (!empty(Yii::$app->user->identity->user_id)): ?>
 
-                    <a class="btn btn-red" title="" href="">提交</a>
+                            <?php
+                            $form = ActiveForm::begin([
+                                'action' => ['sp-offer/create', 'id' => 'P0', 'type' => 'Purchase'],
+                                'method' => 'post',
+                                'id'     => $modelOffer->formName(),
+                            ]);
+                            ?>
+
+                            <p class="right-tar">
+                                <span class="right-color">提交价格 : </span>
+                                <?= $form->field($modelOffer, 'price')->textInput()->label(false) ?>
+                            </p>
+
+                            <p>
+
+                            <hr/>
+
+                            <?=
+                            FileUploadUI::widget([
+                                'model'         => $modelOffer,
+                                'attribute'     => 'path',
+                                'url'           => ['upload/image-upload', 'id' => $modelOffer->path, 'type' => 'sp_offer', 'attribute' => 'path'],
+                                'gallery'       => false,
+                                'fieldOptions'  => [
+                                    'accept' => 'image/*'
+                                ],
+                                'clientOptions' => [
+                                    'maxFileSize'      => 2000000,
+                                    'dataType'         => 'json',
+                                    'maxNumberOfFiles' => 5,
+                                ],
+
+                                // ...
+                                'clientEvents'  => [
+
+                                    'fileuploaddone' => 'function(e, data) {
+                                console.log(e);
+                                console.log(data);
+                                
+                                var html = "";
+                                
+                                var ImagesContent = $("#ImagesContent");
+                                
+                                $.each(data.result.files, function (index, file) {
+                                    html += file.name + \',\';
+                                });
+                                
+                                html += ImagesContent.val();
+                                
+                                ImagesContent.val(html);
+                                
+                                return true;
+                            }',
+                                    'fileuploadfail' => 'function(e, data) {
+                                console.log(e);
+                                console.log(data);
+                            }',
+                                ],
+                            ]);
+                            ?>
+
+                            <?= $form->field($modelOffer, 'path')->textarea(['id' => 'ImagesContent', 'style' => 'display:none;'])->label(false) ?>
+
+                            <?= $form->field($modelOffer, 'offer_id')->hiddenInput(['value' => $model->purchase_id])->label(false) ?>
+
+                            <hr/>
+
+                            </p>
+
+                            <p>
+
+                                <?=
+                                $form->field($modelOffer, 'content')
+                                    ->widget('kucha\ueditor\UEditor', [
+                                        'clientOptions' => [
+                                            //设置语言
+                                            'lang'               => 'zh-cn',
+                                            'initialFrameHeight' => '400',
+                                            'elementPathEnabled' => false,
+                                            'wordCount'          => false,
+                                        ]
+                                    ]);
+                                ?>
+
+                            </p>
+
+                            <p>
+                                <?= Html::submitButton('提交内容', ['class' => 'btn btn-red']) ?>
+                            </p>
+
+                            <br/>
+
+                            <p>
+                                <?= Yii::$app->view->renderFile('@app/views/default/formMsg.php'); ?>
+                            </p>
+
+                            <?php ActiveForm::end(); ?>
+
+                        <?php else: ?>
+
+                            <h3>请登录后提交内容 !! </h3>
+
+                            <br/>
+
+                            点这里, <a href="<?= Url::to(['member/login']) ?>" title="<?= $model->title ?>">用户登录</a>
+
+                        <?php endif; ?>
+
+                    <?php else: ?>
+
+                        <h2>此采购已被采纳 !!</h2>
+
+                    <?php endif; ?>
+
                 </div>
 
             </div>
