f37c757a22d46db40599899897bddeacec671a9d
Yxl <zccem@163.com>
2018-03-19 23:43:48 +0800

    创始室主机上传,架构大改变..2018.03.19

--- backend/views/admin/center/backup.php
+++ backend/views/admin/center/backup.php
@@ -1,15 +1,12 @@
 <?php
 /**
- *
- * 网站SEO设置
- *
  * Created by Yxl.
  * User: <zccem@163.com>.
- * Date: 2018/1/12
- * Time: 10:40
+ * Date: 2018/3/19
+ * Time: 23:34
  */
 
-$this->title = '网站 SEO 配置';
+$this->title = '网站备份';
 $this->params['breadcrumbs'][] = ['label' => '网站配置', 'url' => ['index']];
 
 ?>
